26bcd6b1f5797cb3c1ced27a8ab1556a8f03a56a
Suraj <surajsppokale@gmail.com>
2026-08-24 16:28:52 +0530

    ABout Internship

--- Dsa.java
+++ Dsa.java
@@ -2,6 +2,8 @@ public class Dsa {
     public static void main(String[] args)
     {
         System.out.println("Hello Suraj S P");
+        System.out.println("Suraj S P is doing SDE Internship");
+        
 
     }
 }
