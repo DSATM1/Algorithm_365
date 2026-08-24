ad2ffd8f166751c44d3e38abff3aff04ca7f0d0b
Suraj <surajsppokale@gmail.com>
2026-08-24 13:04:18 +0530

    Day_03 Files and Syntax of Variables and Swappping Program

--- Java/Day_02/Num.java
+++ Java/Day_02/Num.java
@@ -1,6 +1,5 @@
 public class Num {
     public static void main(String a[]){
-        // int x = 1;
         for (int i = 1; i<=10; i++){
             System.out.println(i);
         }
---
+++ Java/Day_03/Syntax.java
@@ -0,0 +1,42 @@
+package Java.Day_03;
+
+public class Syntax 
+{
+    public static void main(String[] args)
+    {
+        // int age = 24;
+        // int year = 2002;
+        // float product = age*year;
+        // System.out.println(product);
+        // float multi = product;
+        // year = 2000;
+        // System.out.println(multi);
+
+        // byte num1 = 10;
+        // byte num2 = num1;
+        
+        // System.out.println(num2);
+
+        // String name ="Suraj";
+        // String full_name = name;
+        // full_name = "Suraj S P";
+        // name = full_name;
+        // System.out.println(name); 
+        // System.out.println(full_name);
+
+        // int n1 = 10;
+        // int n2 = 20;
+        // int temp;
+        // temp = n1;
+        // n1 = n2 ;
+        // n2 = temp ;
+
+        // n1 = n1 + n2;
+        // n2 = n1 - n2;
+        // n1 = n1 - n2;
+        // System.out.println("n1 = " + n1);
+        // System.out.println("n2 = " + n2);
+
+
+    }
+}
