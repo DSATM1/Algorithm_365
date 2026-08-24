b6676b7a163f006fe46548a5cfbc7556273272ba
Suraj <surajsppokale@gmail.com>
2026-08-24 13:32:20 +0530

    Floating Datatype

---
+++ Java/Day_03/Float.java
@@ -0,0 +1,12 @@
+package Java.Day_03;
+
+public class Float {
+    public static void main(String[] args)
+    {
+        float f1 = 30890;
+        float f2 = 32145;
+        float pie = 3.14f;
+        System.out.println(pie);
+        System.out.println(f1/f2);
+    }
+}
