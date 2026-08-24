a842179db8a3c6e3f5d04d0b6a0877df646141cd
Suraj <surajsppokale@gmail.com>
2026-08-24 17:14:03 +0530

    Calculation

--- Datatypes.java
+++ Datatypes.java
@@ -1,5 +1,8 @@
 public class Datatypes {
     public static void main(String [] args){
         System.out.println("Hello Suraj");
+        int a = 10;
+        int b = a;
+        System.out.println(b);
     }
 }
