2755d25e566522144e9bd14b1c2461aed82ac7d4
Suraj <surajsppokale@gmail.com>
2026-08-24 13:40:57 +0530

    Boolean Datatype

---
+++ Java/Day_03/Boolean.java
@@ -0,0 +1,11 @@
+package Java.Day_03;
+
+public class Boolean 
+{
+    public static void main(String [] args)
+    {
+        boolean isMan = true;
+        System.out.println(isMan);
+    }
+    
+}
